eec1da9ae32bf0b37a6d38e37db98eb846fca228
Oscar Ramos <oscarjrs7@gmail.com>
2018-12-04 09:03:40 -0500

    Local new branch being tested.

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -7,5 +7,7 @@ int main()
     std::cout << "Hello World!" << std::endl;
     std::cout << "Hello Git!" << std::endl;
     std::cout << "Hello First Repo!"  << std::endl;
+	std::cout << "Test Local New Branch" << std::endl;
+	
     return 0;
 }
--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # MyFirstHelloWorld
 First Github project
 Now with more branch creation!
+Local branch created
